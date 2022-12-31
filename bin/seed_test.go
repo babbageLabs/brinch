98e5623a00bb3b7b67cbfd9b57d288b8922fcf75
francis.kinuthia <francis.kinuthia@softwaregroup.com>
2022-12-31 17:10:43 +0300

    Add test coverage

--- bin/seed_test.go
+++ bin/seed_test.go
@@ -38,7 +38,7 @@ func TestSeed_ScanInvalidRegex(t *testing.T) {
 	assert.Error(t, err)
 }
 
-func TestSeed_SeedSuccess(t *testing.T) {
+func TestSeed_SeedSequentialSuccess(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	assert.NoError(t, err)
 	defer func(db *sql.DB) {
@@ -65,6 +65,39 @@ func TestSeed_SeedSuccess(t *testing.T) {
 		FileMatchPattern: "^expected\\.sql$",
 		DB:               db,
 		Mode:             Sequential,
+	}
+
+	_, e = seed.Seed()
+	assert.NoError(t, e)
+}
+
+func TestSeed_SeedBatchedSuccess(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			Logger.Fatal(err)
+		}
+	}(db)
+
+	// Initialize SqlFile
+	s := NewSQLFile()
+	e := s.LoadFile("../testdata/expected.sql")
+	assert.NoError(t, e)
+	// before we actually execute our api function, we need to expect required DB actions
+	//var lastInsertID, affected int64
+	//rows := sqlmock.NewResult(lastInsertID, affected)
+	//mock.ExpectBegin()
+	//mock.ExpectExec(s.queries[0]).WillReturnResult(rows)
+	//mock.ExpectCommit()
+	mock.ExpectClose()
+
+	seed := &Seed{
+		Path:             "../testdata",
+		FileMatchPattern: "^expected\\.sql$",
+		DB:               db,
+		Mode:             Batched,
 	}
 
 	_, e = seed.Seed()
